cb0ce64edaf4f1b8c1b98b3d83b363dd45cee1e4
EmsRibeiro <jrc.emerson@gmail.com>
2026-04-24 16:54:03 -0300

    refactor: adicionada rota de img e requerimento para acessar o Multer

--- src/routes/professional.routes.js
+++ src/routes/professional.routes.js
@@ -2,7 +2,16 @@ const express = require("express");
 const router = express.Router();
 const professionalController = require("../controllers/professional.controller");
 
-router.post("/", professionalController.createProfessional);
+// import do middleware das img
+const upload = require("../middlewares/upload.middleware");
+
+// colocada a img no endpoint para acessar pelo front
+router.post(
+  "/",
+  upload.single("image"),
+  professionalController.createProfessional,
+);
+
 router.get("/:id", professionalController.getProfessionalById);
 router.put("/:id", professionalController.updateProfessional);
 
